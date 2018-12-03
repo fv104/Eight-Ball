1876398b2b0c62e9d7bdb6180b3e3cf1c5dea381
fv104 <fv104@hotmail.com>
2018-12-03 10:23:40 +0100

    Contenido añadido a bola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Eight-ball';
+  title = 'Bola 8';
+
+    answers = [
+        { type: "afirmative", text: "En mi opinión, sí" },
+        { type: "afirmative", text: "Es cierto" },
+        { type: "afirmative", text: "Es decididamente así" },
+        { type: "afirmative", text: "Probablemente" },
+        { type: "afirmative", text: "Buen pronóstico" },
+        { type: "afirmative", text: "Todo apunta a que sí" },
+        { type: "afirmative", text: "Sin duda" },
+        { type: "afirmative", text: "Sí" },
+        { type: "afirmative", text: "Sí - definitivamente" },
+        { type: "afirmative", text: "Debes confiar en ello" },
+        { type: "doubt", text: "Respuesta vaga, vuelve a intentarlo" },
+        { type: "doubt", text: "Pregunta en otro momento" },
+        { type: "doubt", text: "Será mejor que no te lo diga ahora" },
+        { type: "doubt", text: "No puedo predecirlo ahora" },
+        { type: "doubt", text: "Concéntrate y vuelve a preguntar" },
+        { type: "negative", text: "No cuentes con ello" },
+        { type: "negative", text: "Mi respuesta es no" },
+        { type: "negative", text: "Mis fuentes me dicen que no" },
+        { type: "negative", text: "Las perspectivas no son buenas" },
+        { type: "negative", text: "Muy dudoso" }
+        ]
+    currentQuestion = "";
+    responses = [];
+    newQuestion(){
+
+      const currentQuestion = this.currentQuestion.trim();
+      if(currentQuestion !== ""){
+          const randomIndex = Math.floor(Math.random() * this.answers.length);
+
+          const newAnswer = {
+              question: currentQuestion,
+              answer: this.answers[randomIndex]
+          };
+          this.responses.unshift(newAnswer);
+      }
+    }
 }
